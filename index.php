293b7a0a399664214f869d6261d8207a7db4b20c
Juan <jmrgjuan@gmail.com>
2025-09-27 16:20:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,11 @@
 
 declare(strict_types=1);
 
-echo "¡Hola tío, esto es Northflank con PHP!";
+// test database
+$databaseUri = getenv('DB_POSTGRES_URI');
+$conn = pg_connect($databaseUri);
+
+$msg = $conn? "database connection OK" : "Error de conexión a PostgreSQL";
+
+echo "<h1>¡Hola tío, esto es Northflank con PHP!</h1>";
+echo "<h2>$msg</h2>";
